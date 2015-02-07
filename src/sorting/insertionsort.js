3aa14b15711331c57e3b30eda032d0d8b6090ebf
Vilmos Ioo <vilmosi@blinkbox.com>
2015-02-07 23:54:29 +0000

    Bug fix in sorting helper

--- src/sorting/insertionsort.js
+++ src/sorting/insertionsort.js
@@ -1,12 +1,23 @@
 'use strict';
 
 /*
-* Logic: 
-* Advantages: 
-* Disadvantage: 
+* Logic: For each element in an array, find the position it belongs in and insert it in the correct location.
+* Uses a growing sorted subset of the array.
+* At each array-position, it checks the value against the largest value in the sorted list (which happens to be next to it, in the previous array-position checked). If larger, it leaves the element in place and moves to the next. If smaller, it finds the correct position within the sorted list, shifts all the larger values up to make a space, and inserts into that correct position.
+* Advantages: Effitient on small datasets
+* Disadvantage: Slow compared to more advanced sorting algorithms
 */
 
 module.exports = function(arr){
-	
+	var x, i, j, l = arr.length;
+	for(i = 1; i < l; i++){
+		x = arr[i];
+		j = i - 1;
+		while(x < arr[j] && j > 0){
+			arr[j+1] = arr[j];
+			j--;
+		}
+		arr[j] = x;
+	}
 	return arr;
 };
--- test/helpers/sorting.js
+++ test/helpers/sorting.js
@@ -3,21 +3,17 @@
 var expect = require('expect.js'),
 	data = [[], [1], [2, 1], [3, 2, 1], [1, 2, 3, 4], [9, 6, 8, 4, 2, 3, 1, 11, 0, 5, 5, -1], [5, 4, 3, 2, 1]],
 	// we generate the sorting tests by using the array of inputs and calculating the expected sorted value using the native sort method
-	tests = data.reduce(function(obj, test){
-		obj.push({
+	tests = data.map(function(test){
+		return {
 			input: test,
-			expect: test.slice().sort(function(a, b){
-				return a < b;
+			expect: test.slice(0).sort(function(a, b){
+				return a - b;
 			})
-		});
-		return obj;
-	}, []);
+		};
+	});
 
 module.exports = function(sort){
 	tests.forEach(function(test){
-		console.log(test.input);
-		console.log(test.expect);
-
-		expect(sort(test.input)).to.eql(test.expect);
+		expect(sort(test.input.slice(0))).to.eql(test.expect);
 	});
 };
