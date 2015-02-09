551b822b403a3000f854ab5f49dac8c2bf8973a5
Vilmos Ioo <vilmosi@blinkbox.com>
2015-02-09 16:30:56 +0000

    Init merge

--- src/sorting/mergesort.js
+++ src/sorting/mergesort.js
@@ -1,12 +1,33 @@
 'use strict';
 
 /*
-* Logic: 
-* Advantages: 
+* Logic: Divide the array into n sublists, each with 1 element initially. Repeatedly merge sublists until you only have 1 sublist.
+* Advantages: Quick O(n lgn)
 * Disadvantage: 
 */
 
+function mergesort(arr){
+	var l = arr.length, middle = Math.floor(l / 2), left = [], right = [];
+
+	// an array of size 1 is considered sorted
+	if(l === 1){
+		return m;
+	}
+
+	// generate two equal sized arrays
+	left = arr.slice(0, middle);
+	right = arr.slice(middle, l);
+
+	// recursively sort both sublists
+	left = merge_sort(left);
+	left = merge_sort(left);
+	
+
+};
+
 module.exports = function(arr){
 	
+	var l = arr.length, left = [];
+
 	return arr;
 };
