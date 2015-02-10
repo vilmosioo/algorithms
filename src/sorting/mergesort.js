3938cfb65669e88e4404a3b3b3809fb3620f3aaa
Vilmos Ioo <cool.villi@gmail.com>
2015-02-10 09:50:44 +0000

    Merge sort complete

--- src/sorting/mergesort.js
+++ src/sorting/mergesort.js
@@ -6,28 +6,37 @@
 * Disadvantage: 
 */
 
+function merge(left, right){
+	var i, j, result = [];
+	while(left.length && right.length){
+		if(left[0] < right[0]){
+			result.push(left.shift());
+		} else {
+			result.push(right.shift());
+		}
+	}
+	// we might still have elements left
+	return result.concat(left).concat(right);
+}
+
 function mergesort(arr){
 	var l = arr.length, middle = Math.floor(l / 2), left = [], right = [];
-
+	
 	// an array of size 1 is considered sorted
-	if(l === 1){
-		return m;
+	if(l <= 1){
+		return arr;
 	}
-
+	
 	// generate two equal sized arrays
 	left = arr.slice(0, middle);
 	right = arr.slice(middle, l);
 
 	// recursively sort both sublists
-	left = merge_sort(left);
-	left = merge_sort(left);
-	
-
+	left = mergesort(left);
+	right = mergesort(right);
+	return merge(left, right);
 };
 
 module.exports = function(arr){
-	
-	var l = arr.length, left = [];
-
-	return arr;
+	return mergesort(arr);
 };
