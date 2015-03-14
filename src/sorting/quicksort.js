2b35d5a016409c1af61a0df4bd7e4c0119789654
Vilmos Ioo <cool.villi@gmail.com>
2015-03-14 11:08:32 +0000

    Quicksort description

--- src/sorting/quicksort.js
+++ src/sorting/quicksort.js
@@ -1,9 +1,10 @@
 'use strict';
 
 /*
-* Logic: 
-* Advantages: 
-* Disadvantage: 
+* Efficient sorting algorithm. Can be 2-3 times faster than mergesort
+* Logic: Divide and conquer algorithm. Pick an element (pivot) from the array. Partition the array into 2 sublists: everything smaller than the pivot to the left, everythigng else to the right. Apply this recursively to the sublists.
+* Advantages: O(n lgn)
+* Disadvantage: Worst case On^2 when pivot is left most elemetn and array is already sorted
 */
 
 module.exports = function(arr){
